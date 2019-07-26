6d5d3185eb6d4967c5bf419ed260502d8a80f21a
Jack Fisher <jackfisher2012@gmail.com>
2019-07-26 14:51:33 +0100

    GYE-1873: Add base view spec

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,11 +13,11 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-play-26" % "0.42.0" % Test classifier "tests",
-    "org.scalatest" %% "scalatest" % "3.0.8" % "test",
-    "org.jsoup" % "jsoup" % "1.10.2" % "test",
+    "org.scalatest" %% "scalatest" % "3.2.0-SNAP10" % "test",
+    "org.jsoup" % "jsoup" % "1.12.1" % "test",
     "com.typesafe.play" %% "play-test" % current % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % "test, it",
     "org.mockito" % "mockito-core" % "3.0.0" % "test,it"
   )
 
