6c469e3fd84ee5741f4a29b047c416fa55aa4f39
kuldeep Wadhwa <kuldeepwadhwa9@gmail.com>
2019-08-12 13:45:07 +0100

    added tests for singout controller

--- test/uk/gov/hmrc/customs/emailfrontend/unit/controllers/SignOutControllerSpec.scala
+++ test/uk/gov/hmrc/customs/emailfrontend/unit/controllers/SignOutControllerSpec.scala
@@ -16,19 +16,29 @@
 
 package uk.gov.hmrc.customs.emailfrontend.unit.controllers
 
+import play.api.mvc.Session
 import play.api.test.Helpers._
-import uk.gov.hmrc.customs.emailfrontend.controllers.SignOutController
+import uk.gov.hmrc.customs.emailfrontend.controllers.{ApplicationController, SignOutController}
+import uk.gov.hmrc.customs.emailfrontend.views.html.start_page
 
 class SignOutControllerSpec extends ControllerSpec {
 
   val controller = new SignOutController(fakeAction)
 
   "SignOut Controller" should {
-    "return have status See_Other (303) and redirect to feedback survey" in withAuthorisedUserWithoutEnrolments{
+    "return have status See_Other (303) and redirect to feedback survey" in withAuthorisedUserWithoutEnrolments {
       val result = controller.signOut(request)
       status(result) shouldBe SEE_OTHER
-      redirectLocation(result) should contain("/feedback/CDS")
+      redirectLocation(result).value should endWith("/feedback/CDS")
     }
-  }
 
+    "clear the session once the user signs out" in withAuthorisedUserWithoutEnrolments {
+      status(controller.signOut(request)) shouldBe SEE_OTHER
+      val view = app.injector.instanceOf[start_page]
+      val startPageController = new ApplicationController(fakeAction, view)
+      val result = startPageController.show(request)
+      session(result) shouldBe Session.emptyCookie
+    }
+
+  }
 }
