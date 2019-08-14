4c51fa2eea390585e73f2acfeafb3f49244d9be7
Kapil Sanghvi <kapil.sanghvi@digital.hmrc.gov.uk>
2019-08-14 16:52:47 +0100

    GYE-1915 Added acceptance setup

---
+++ test/uk/gov/hmrc/customs/emailfrontend/acceptance/pages/BasePage.scala
@@ -0,0 +1,9 @@
+package uk.gov.hmrc.customs.emailfrontend.acceptance.pages
+
+import org.openqa.selenium.WebDriver
+import org.scalatest.Matchers
+import uk.gov.hmrc.webdriver.SingletonDriver
+
+class BasePage extends Matchers {
+
+}
---
+++ test/uk/gov/hmrc/customs/emailfrontend/acceptance/specs/BaseSpec.scala
@@ -0,0 +1,7 @@
+package uk.gov.hmrc.customs.emailfrontend.acceptance.specs
+
+import org.scalatest.{FeatureSpec, GivenWhenThen}
+
+trait BaseSpec extends FeatureSpec with GivenWhenThen{
+
+}
