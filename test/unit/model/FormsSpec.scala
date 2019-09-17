b20c76b97a1d7b066eab0a3be906087e157c5155
Kapil Sanghvi <kapil.sanghvi@digital.hmrc.gov.uk>
2019-09-17 16:25:26 +0100

    Updated/added tests for forms and error handler

---
+++ test/unit/model/FormsSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.model
+
+import org.scalatest.Matchers._
+import org.scalatestplus.play.PlaySpec
+import uk.gov.hmrc.customs.emailfrontend.forms.Forms
+import uk.gov.hmrc.customs.emailfrontend.model.{Email, YesNo}
+
+class FormsSpec extends PlaySpec {
+
+  "Forms" should {
+    "bind/unbind the emailForm data correctly" in {
+      val bind = Forms.emailForm.mapping.bind(Map("email" -> "test@email.com"))
+      bind shouldBe Right(Email("test@email.com"))
+      val unbind = Forms.emailForm.mapping.unbind(Email("test@email.com"))
+      unbind shouldBe Map("email" -> "test@email.com")
+    }
+
+    "bind/unbind the confirmEmailForm data correctly" in {
+      val bind = Forms.confirmEmailForm.mapping.bind(Map("isYes" -> "true"))
+      bind shouldBe Right(YesNo(Some(true)))
+      val unbind = Forms.confirmEmailForm.mapping.unbind(YesNo(Some(true)))
+      unbind shouldBe Map("isYes" -> "true")
+    }
+  }
+}
