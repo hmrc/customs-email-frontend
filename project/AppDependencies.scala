6756abf6087439d1bff680c21ba8b17aac954dc9
Kapil Sanghvi <kapil.sanghvi@digital.hmrc.gov.uk>
2019-10-30 16:09:41 +0000

    GYE-2010 updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "govuk-template" % "5.36.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template" % "5.43.0-play-26",
     "uk.gov.hmrc" %% "play-ui" % "8.3.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "8.4.0-play-26",
     "uk.gov.hmrc" %% "bootstrap-play-26" % "0.42.0",
@@ -20,6 +20,6 @@ object AppDependencies {
     "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it",
     "org.mockito" % "mockito-core" % "3.1.0" % "test,it",
-    "com.github.tomakehurst" % "wiremock-standalone" % "2.23.2" % "test"
+    "com.github.tomakehurst" % "wiremock-standalone" % "2.25.1" % "test"
   )
 }
