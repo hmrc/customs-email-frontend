1c4b9da344e5af88dd4786ae3997577bf460af5f
Gaurav Seth <mailtogauravseth@gmail.com>
2021-09-10 11:47:56 +0100

    GYE-2654: DAST build job for manage email service

---
+++ jenkins-run-zap-managemail-local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# For testing CUSTOMS_ROSM_FRONTEND
+# sm --start CUSTOMS_ROSM_FRONTEND_ALL -f (downloads and starts latest versions of all apps)
+# sm --stop  CUSTOMS_ROSM_FRONTEND
+# sbt -Dapplication.router=testOnlyDoNotUseInAppConf.Routes "run 9830" (start customs-rosm-frontend from local source)
+
+sbt -Dbrowser=remote-chrome -Dzap.proxy=true -Dport=9898 manageemail:test
+
+
