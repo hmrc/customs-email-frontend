da65f60ed2f637a62c5f9914a0c4d1daff346c84
muhammadibraaheem <46523489+muhammadibraaheem@users.noreply.github.com>
2021-10-20 16:46:37 +0100

    added reverse tabnapping preventer component

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,6 +6,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "govuk-template" % "5.68.0-play-28",
     "uk.gov.hmrc" %% "play-ui" % "9.4.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.9.0-play-28",
     "uk.gov.hmrc" %% "http-caching-client" % "9.3.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.6.0",
     "uk.gov.hmrc" %% "emailaddress" % "3.5.0",
