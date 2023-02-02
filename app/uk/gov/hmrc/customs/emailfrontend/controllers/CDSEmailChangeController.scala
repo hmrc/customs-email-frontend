b61a91125d20af77110443258f5a05885128d1b5
bsgangapat <106598990+bsgangapat@users.noreply.github.com>
2023-02-02 23:24:01 +0000

    MMCA-3765 created CDS Declaration service for change email address

---
+++ app/uk/gov/hmrc/customs/emailfrontend/controllers/CDSEmailChangeController.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.customs.emailfrontend.controllers
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import uk.gov.hmrc.customs.emailfrontend.config.AppConfig
+import uk.gov.hmrc.customs.emailfrontend.forms.Forms.emailForm
+import uk.gov.hmrc.customs.emailfrontend.views.html.change_cds_email
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class CDSEmailChangeController @Inject()(view: change_cds_email,
+                                      mcc: MessagesControllerComponents)
+                                     (implicit override val messagesApi: MessagesApi, appConfig: AppConfig)
+  extends FrontendController(mcc) with I18nSupport {
+
+  def show: Action[AnyContent] = Action { implicit request =>
+    Ok(view(emailForm))
+  }
+}
