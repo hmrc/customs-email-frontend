d7840c24780bca1064727e94c9d41150965ee1fb
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-04-14 06:07:42 +0100

    MMCA-3890 : Upgrade from Java 8 to 11

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.15.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.3.0-play-28",
     "uk.gov.hmrc" %% "http-caching-client" % "10.0.0-play-28",
     "uk.gov.hmrc" %% "emailaddress" % "3.8.0",
