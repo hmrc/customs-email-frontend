d5690544351e6173ae64169f47964a58050548e9
muhammadibraaheem <46523489+muhammadibraaheem@users.noreply.github.com>
2023-10-03 20:39:37 +0100

    updated deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.19.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.22.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
     "uk.gov.hmrc" %% "emailaddress" % "3.8.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.15.2",
@@ -13,10 +13,10 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.19.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.22.0" % Test,
     "org.jsoup" % "jsoup" % "1.16.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
     "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % "test, it",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28"
   )
 }
