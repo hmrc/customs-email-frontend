7b6d9f2ce9bcded2e1ba8807c1a6668b3d888d9c
HariHmrc <149591728+HariHmrc@users.noreply.github.com>
2024-03-05 15:09:38 +0000

    MMCA-4469 : upgraded libs version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.22.0"
+  private val bootstrapVersion = "8.4.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.23.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-29" % bootstrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-29" % "8.1.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-29" % "1.6.0",
     "uk.gov.hmrc" %% "emailaddress" % "3.8.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.15.2",
     "org.typelevel" %% "cats-core" % "2.9.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
-    "org.jsoup" % "jsoup" % "1.16.1" % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % "test, it"
+    "uk.gov.hmrc" %% "bootstrap-test-play-29" % bootstrapVersion % Test,
+    "org.jsoup" % "jsoup" % "1.17.2" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.64.8" % "test, it",
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.30" % "test, it"
   )
 }
