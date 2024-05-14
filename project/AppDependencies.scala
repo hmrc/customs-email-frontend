3d87a124a6c44eea1185864e69e95796f99067ff
Adam Cooper <adam.cooper2@digital.hmrc.gov.uk>
2024-05-14 11:48:20 +0100

    MMCA-4670-update the plugin versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,9 +7,9 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.1.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "9.10.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % "1.9.0",
-    "uk.gov.hmrc" %% "emailaddress" % "3.8.0",
+    "uk.gov.hmrc" %% "emailaddress-play-30" % "4.0.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.17.0",
     "org.typelevel" %% "cats-core" % "2.10.0"
   )
