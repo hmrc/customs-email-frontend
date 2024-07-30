9bcc366fa4403918450210ceca387587bb71f53c
HariHmrc <149591728+HariHmrc@users.noreply.github.com>
2024-07-30 01:03:26 +0100

    MMCA-4773: platops bot comment fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
