096d5e2bcf65c04c41d6e9b92d9c4282b4c63c86
Adam Cooper <61467301+AdamCoops@users.noreply.github.com>
2025-01-07 12:50:31 +0000

    MMCA-5084-Update backlinks for update email ui flow (#248)
    * MMCA-5084-Update backlinks for update email ui flow
    
    * MMCA-5084-update back links to not include full routes and push first draft of tests
    
    * MMCA-5084-Update tests
    
    * MMCA-5084-update tests to match scala style
    
    * MMCA-5804-update test names and view labels for tests, and refactor backlink test to check specific link
    
    * MMCA-5084-update unit tests to make use of a viewtestHelper for repeated UI tests
    
    * MMCA-5084-update helper location, remove specbases and add param
    
    * MMCA-5084-update param name

---
+++ test/uk/gov/hmrc/customs/emailfrontend/utils/ViewTestHelper.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.customs.emailfrontend.utils
+
+import org.jsoup.nodes.Document
+import org.scalatest.Assertion
+import org.scalatest.matchers.must.Matchers.mustBe
+
+trait ViewTestHelper extends SpecBase {
+
+  def shouldContainCorrectServiceUrls(viewDoc: String, url: String): Assertion = {
+    viewDoc.contains(url) mustBe true
+    viewDoc.contains("/accessibility-statement/manage-email-cds") mustBe true
+  }
+
+  def shouldContainBackLinkUrl(viewDoc: Document, url: String): Assertion = {
+    viewDoc.getElementsByClass("govuk-back-link").text() mustBe "Back"
+    viewDoc.html().contains(url) mustBe true
+  }
+
+  def shouldContainCorrectBanners(viewDoc: Document): Assertion =
+    viewDoc
+      .getElementsByClass("govuk-phase-banner")
+      .text() mustBe "BETA This is a new service – your feedback will help us to improve it."
+}
---
+++ test/uk/gov/hmrc/customs/emailfrontend/views/ChangeYourEmailSpec.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.customs.emailfrontend.views
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.scalatest.matchers.must.Matchers.mustBe
+import play.api.Application
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import uk.gov.hmrc.customs.emailfrontend.config.AppConfig
+import uk.gov.hmrc.customs.emailfrontend.forms.Forms.emailForm
+import uk.gov.hmrc.customs.emailfrontend.utils.Utils.emptyString
+import uk.gov.hmrc.customs.emailfrontend.utils.{FakeIdentifierAgentAction, ViewTestHelper}
+import uk.gov.hmrc.customs.emailfrontend.views.html.change_your_email
+import uk.gov.hmrc.customs.emailfrontend.controllers.routes
+
+class ChangeYourEmailSpec extends ViewTestHelper {
+
+  "ChangeYourEmail" should {
+    "display correct guidance" when {
+
+      "banner is visible" in new Setup {
+        shouldContainCorrectBanners(view)
+      }
+
+      "title is visible" in new Setup {
+        shouldContainCorrectTitle(view)
+      }
+
+      "service urls are visible" in new Setup {
+        shouldContainCorrectServiceUrls(view.html(), routes.SignOutController.signOut.url)
+      }
+
+      "backlink is visible" in new Setup {
+        shouldContainBackLinkUrl(view, routes.VerifyChangeEmailController.create.url)
+      }
+    }
+  }
+
+  private def shouldContainCorrectTitle(viewDoc: Document, title: String = emptyString) =
+    if (title.nonEmpty) {
+      viewDoc.title() mustBe title
+    } else {
+      viewDoc.title() mustBe "Enter a new email address"
+    }
+
+  trait Setup {
+    val app: Application = applicationBuilder[FakeIdentifierAgentAction]()
+      .configure("play.filters.csrf.enabled" -> "false")
+      .build()
+
+    implicit val request: FakeRequest[AnyContentAsEmpty.type] =
+      fakeRequestWithCsrf("GET", "/some/resource/path")
+
+    implicit val msgs: Messages = app.injector.instanceOf[MessagesApi].preferred(fakeRequest(emptyString, emptyString))
+
+    implicit val appConfig: AppConfig = app.injector.instanceOf[AppConfig]
+
+    val view: Document =
+      Jsoup.parse(
+        app.injector
+          .instanceOf[change_your_email]
+          .apply(emailForm, appConfig)(request = request, messages = msgs)
+          .body
+      )
+  }
+}
