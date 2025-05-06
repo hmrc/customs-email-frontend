11d496c156bba92c49d667de8f0bd3f6fc765710
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2025-05-06 01:12:45 +0100

    [MMCA-5274] Fix PlatOps warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.8.0"
+  val bootstrapVersion = "9.11.0"
 
   private val excludeHttpVerbs = ExclusionRule(organization = "uk.gov.hmrc", name = "http-verbs-play-30")
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion excludeAll excludeHttpVerbs,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.11.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.13.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % "2.4.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.17.0",
     "org.typelevel"                %% "cats-core"                  % "2.10.0"
