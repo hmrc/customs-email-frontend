c79bcc662006ed739db6befd2a66da7f63b7e46d
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2025-05-06 01:18:14 +0100

    [MMCA-5274] Fix PlatOps warning for mongo-play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion excludeAll excludeHttpVerbs,
     "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.13.0",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % "2.4.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % "2.6.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.17.0",
     "org.typelevel"                %% "cats-core"                  % "2.10.0"
   )
