583d603039ed266abc2a145aa62cb53b37d934f0
Tom Dennis <207199298+tomndennis@users.noreply.github.com>
2025-08-27 17:34:32 +0100

    MIDVA UI GOV.UK Rebrand (#257)
    Co-authored-by: tomndennis <tomndennis@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.14.0"
+  val bootstrapVersion = "10.1.0"
 
   private val excludeHttpVerbs = ExclusionRule(organization = "uk.gov.hmrc", name = "http-verbs-play-30")
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion excludeAll excludeHttpVerbs,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.13.0",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % "2.6.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % "2.7.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.17.0",
     "org.typelevel"                %% "cats-core"                  % "2.10.0"
   )
