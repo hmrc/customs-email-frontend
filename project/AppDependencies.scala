fb287550320a12ed523853cbb0c5a2cb13c994ca
Tom Dennis <207199298+tomndennis@users.noreply.github.com>
2026-06-03 12:06:36 +0100

    DCWL-4730 - play-frontend-hmrc v13 upgrade (#264)
    Co-authored-by: tomndennis <tomndennis@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,15 +9,15 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion excludeAll excludeHttpVerbs,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.32.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "13.8.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % "2.12.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.17.0",
-    "org.typelevel"                %% "cats-core"                  % "2.10.0"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.21.4",
+    "org.typelevel"                %% "cats-core"                  % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion % Test excludeAll excludeHttpVerbs,
-    "org.jsoup"          % "jsoup"                  % "1.17.2"         % Test,
+    "org.jsoup"          % "jsoup"                  % "1.22.2"         % Test,
     "org.scalatestplus" %% "mockito-4-11"           % "3.2.18.0"       % Test
   )
 }
